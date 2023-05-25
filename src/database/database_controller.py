631792fdd13a0e29958e755c89fa2536cfc09498
Hafurfur <rogmesec@yandex.ru>
2023-05-25 22:08:32 +0300

    add engine sql

--- src/database/database_controller.py
+++ src/database/database_controller.py
@@ -1,10 +1,20 @@
-from settings import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+from settings import DB_TYPE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 from src.database.model_db import Base
 
 from sqlalchemy import URL, create_engine, text
 
 
 class DatabaseController:
+    _engine = None
+
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls, *args, **kwargs)
+        if instance._engine is None:
+            _db_url = URL.create(DB_TYPE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+            instance._engine = create_engine(_db_url, echo=True)
+        return instance
+
+
     @staticmethod
     def create_db():
         db_url = URL.create('postgresql', DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
@@ -25,5 +35,6 @@ class DatabaseController:
 
 
 if __name__ == '__main__':
-    # DatabaseController.create_table()
+    DatabaseController.create_db()
+    DatabaseController.create_table()
     pass
