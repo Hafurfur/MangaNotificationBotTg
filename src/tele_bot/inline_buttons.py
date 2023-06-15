fb05088ad034165dc218f81562b159f5bf88b751
Hafurfur <rogmesec@yandex.ru>
2023-06-15 20:19:01 +0300

    add new inline button

--- src/tele_bot/inline_buttons.py
+++ src/tele_bot/inline_buttons.py
@@ -2,5 +2,8 @@ from telebot.util import quick_markup
 
 search_manga_acc_inline = quick_markup({'←': {'callback_data': 'prev_manga_acc'},
                                         '→': {'callback_data': 'next_manga_acc'},
-                                        '✅': {'callback_data': 'add_manga_acc'},
-                                        '❌': {'callback_data': 'cancel_search_manga_acc'}}, row_width=2)
+                                        'Добавить': {'callback_data': 'add_manga_acc'},
+                                        'Отменить': {'callback_data': 'cancel_search_manga_acc'}}, row_width=2)
+
+checking_manga_account_inline = quick_markup({'✅': {'callback_data': 'accept_checking_manga_acc'},
+                                              '❌': {'callback_data': 'cancel_checking_manga_acc'}}, row_width=2)
