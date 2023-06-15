e9c2887b147cfaa5555b8ee8a52a61a5d5dafd75
Hafurfur <rogmesec@yandex.ru>
2023-06-15 20:19:54 +0300

    refactor, change relationship table

--- src/database/models/manga_accounts.py
+++ src/database/models/manga_accounts.py
@@ -1,12 +1,12 @@
+from __future__ import annotations
 from typing import TYPE_CHECKING
-from datetime import datetime
 from .base import Base
 
 from sqlalchemy import DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
-    from .mg_trek_acc_association_table import MgAccountTrackedMgAssociationTable
+    from .tracked_manga import TrackedManga
     from .telegram_accounts import TelegramAccounts
 
 
@@ -14,14 +14,14 @@ class MangaAccounts(Base):
     __tablename__ = 'manga_accounts'
 
     account_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
-    create_date: Mapped[datetime] = mapped_column(DateTime(timezone=False), nullable=False, server_default=func.now())
-    update_date: Mapped[datetime] = mapped_column(DateTime(timezone=False), nullable=False, server_default=func.now())
+    create_date: Mapped[DateTime] = mapped_column(DateTime(timezone=False), nullable=False, server_default=func.now())
+    update_date: Mapped[DateTime] = mapped_column(DateTime(timezone=False), nullable=False, server_default=func.now())
     username: Mapped[str] = mapped_column(nullable=False)
     active: Mapped[bool] = mapped_column(nullable=False)
 
-    telegram_accounts: Mapped[list['TelegramAccounts']] = relationship(back_populates='manga_account')
-    readable_manga: Mapped[list['MgAccountTrackedMgAssociationTable']] = relationship(
-        back_populates='manga_account')
+    telegram_accounts: Mapped[list[TelegramAccounts]] = relationship(back_populates='manga_account')
+    readable_manga: Mapped[list[TrackedManga]] = relationship(secondary='mg_acc_tr_mg_assoc',
+                                                              back_populates='manga_accounts')
 
     def __repr__(self):
         return self
