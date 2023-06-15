d2fb484a598299487a4e5d0349d35669a3a2b855
Hafurfur <rogmesec@yandex.ru>
2023-06-15 20:21:07 +0300

    refactor, change column table, delete relationship

--- src/database/models/mg_trek_acc_association_table.py
+++ src/database/models/mg_trek_acc_association_table.py
@@ -1,13 +1,7 @@
-from typing import TYPE_CHECKING
-from datetime import datetime
 from .base import Base
 
-from sqlalchemy import ForeignKey, DateTime, func
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-if TYPE_CHECKING:
-    from .manga_accounts import MangaAccounts
-    from .tracked_manga import TrackedManga
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class MgAccountTrackedMgAssociationTable(Base):
@@ -15,15 +9,9 @@ class MgAccountTrackedMgAssociationTable(Base):
 
     mg_acc_id: Mapped[int] = mapped_column(ForeignKey('manga_accounts.account_id'), primary_key=True)
     tracked_mg_id: Mapped[int] = mapped_column(ForeignKey('tracked_manga.manga_id'), primary_key=True)
-    create_date: Mapped[datetime] = mapped_column(DateTime(timezone=False), nullable=False, server_default=func.now())
-    update_date: Mapped[datetime] = mapped_column(DateTime(timezone=False), nullable=False, server_default=func.now())
-
-    tracked_manga: Mapped['TrackedManga'] = relationship(back_populates='manga_accounts')
-    manga_account: Mapped['MangaAccounts'] = relationship(back_populates='readable_manga')
 
     def __repr__(self):
         return self
 
     def __str__(self):
-        return f'MgAccountTrackedMgAssociationTable(mg_acc_id={self.mg_acc_id}, tracked_mg_id={self.tracked_mg_id}), ' \
-               f'create_date={self.create_date}, update_date={self.update_date}'
+        return f'MgAccountTrackedMgAssociationTable(mg_acc_id={self.mg_acc_id}, tracked_mg_id={self.tracked_mg_id})'
