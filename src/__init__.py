a31e576d0502449f39df328d38170cf2598b3ba1
Hafurfur <rogmesec@yandex.ru>
2023-08-23 19:17:55 +0300

    Удаление ненужных импортов

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-from src import database
-from src import site_data
-from src import scheduler
-from src import tele_bot
--- src/tele_bot/__init__.py
+++ src/tele_bot/__init__.py
@@ -1,4 +1,2 @@
-from .bot_controller import star_bot
-from .bot_controller import stop_bot
 from .handlers import add_new_mg_acc_handler
 from .handlers import start_bot_handler
