c554bb53cd8c61b34e673169ded6bba80f3032bd
Hafurfur <rogmesec@yandex.ru>
2023-08-23 19:28:38 +0300

    Удален лишний шаблон

--- src/tele_bot/bot_back/inline_buttons.py
+++ src/tele_bot/bot_back/inline_buttons.py
@@ -4,6 +4,3 @@ search_manga_acc_inline = quick_markup({'←': {'callback_data': 'prev_manga_acc
                                         '→': {'callback_data': 'next_manga_acc'},
                                         'Добавить': {'callback_data': 'add_manga_acc'},
                                         'Отменить': {'callback_data': 'cancel_search_manga_acc'}}, row_width=2)
-
-checking_manga_account_inline = quick_markup({'✅': {'callback_data': 'accept_checking_manga_acc'},
-                                              '❌': {'callback_data': 'cancel_checking_manga_acc'}}, row_width=2)
