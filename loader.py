977be3b34fde7f5da4f694f6e30ac1cadcc1501e
Hafurfur <rogmesec@yandex.ru>
2023-08-23 19:29:39 +0300

    Создан файл, добавлено создание главных управляющих объектов

---
+++ loader.py
@@ -0,0 +1,13 @@
+from os import getenv
+from src.database import DatabaseController, SqliteDB
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from sqlalchemy.orm import sessionmaker
+from telebot import TeleBot, StatePickleStorage
+
+
+db = DatabaseController(SqliteDB(getenv('SQLITE_DB_NAME')))
+db.create_table()
+Session_db = sessionmaker(bind=db.get_engine())
+bot = TeleBot(token=getenv('TOKEN'), skip_pending=True, state_storage=StatePickleStorage())
+scheduler = BackgroundScheduler()
