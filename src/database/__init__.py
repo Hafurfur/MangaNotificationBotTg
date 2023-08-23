e0bb2c613e1d2368772aa0256f04291e64d4a1b1
Hafurfur <rogmesec@yandex.ru>
2023-08-23 19:15:55 +0300

    Удаление лишних импортов и перенос создания DatabaseController в loader

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,9 +1,6 @@
+from .sqlite_db import SqliteDB
 from .database_controller import DatabaseController
 from .models import TrackedManga
 from .models import MangaAccounts
 from .models import TelegramAccounts
-from .bot_db import save_telegram_account
-from .bot_db import add_account_tracking
-from .bot_db import having_manga_account
-
-DatabaseController()
+from .models import MgAccountTrackedMgAssociationTable
