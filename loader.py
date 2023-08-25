c40e03ddfe80cd3d35370fd4541367f0a76c1af3
Hafurfur <rogmesec@yandex.ru>
2023-08-26 01:49:21 +0300

    Изменена папка сохранения состояния

--- loader.py
+++ loader.py
@@ -8,5 +8,6 @@ from telebot import TeleBot, StatePickleStorage
 db = DatabaseController(SqliteDB(getenv('SQLITE_DB_NAME')))
 db.create_table()
 Session_db = sessionmaker(bind=db.get_engine())
-bot = TeleBot(token=getenv('TOKEN'), skip_pending=True, state_storage=StatePickleStorage())
+bot = TeleBot(token=getenv('TOKEN'), skip_pending=True,
+              state_storage=StatePickleStorage(file_path='data/state-save/states.pkl'))
 scheduler = BackgroundScheduler()
