98d8357410b51d7cec779e234cba56b91c4b00ff
Hafurfur <rogmesec@yandex.ru>
2023-08-26 01:49:37 +0300

    Добавлено логирование

--- src/scheduler/jobs/new_chapters/manga.py
+++ src/scheduler/jobs/new_chapters/manga.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from src.scheduler.jobs.new_chapters.chapter import Chapter
+from src.logger.base_logger import log
+
 
 if TYPE_CHECKING:
     from bs4 import Tag
@@ -17,13 +19,13 @@ class Manga:
         name: str = release_data.find(class_='link-default').text
         slug: str = release_data.find(class_='link-default').attrs.get('href').split('/')[-1]
         chapters: list[Chapter] = []
-
         soup_all_new_chapters = release_data.find_all(class_='updates__chapter')
 
         for chapter in soup_all_new_chapters:
             new_chapter = Chapter.chapter_builder(chapter)
             if new_chapter.volume >= last_mg_chapter.get('last_volume') and \
                     new_chapter.number > last_mg_chapter.get('last_chapter'):
+                log.debug(f'Новая глава манги "{name}" = {new_chapter}')
                 chapters.append(new_chapter)
 
         return cls(name, slug, chapters)
