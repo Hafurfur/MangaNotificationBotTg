f8388343507d875dc68ffb84b08544488eef6094
Hafurfur <rogmesec@yandex.ru>
2023-09-07 22:54:32 +0300

    Добавление новых настроек, обработка ошибок

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -2,9 +2,22 @@ from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 from src.logger.base_logger import log
 
+try:
+    if Path(Path.cwd(), '.env').exists():
+        if not find_dotenv():
+            log.debug('Не удалось загрузить .env')
+        else:
+            load_dotenv()
+except Exception as error:
+    log.error(f'Ошибка при загрузке переменных окружения {error}')
+    raise
 
-if Path(Path.cwd(), '.env').exists():
-    if not find_dotenv():
-        log.debug('Не удалось загрузить .env')
-    else:
-        load_dotenv()
+# Интервал времени (минуты) задания на отправку уведомлений о выходе новых глав
+NOTIF_JOB_INTERVAL = 5
+
+# Интервал времени (часы) задания на обновление списка "Читаю"
+UPD_READABLE_MB_JOB_INTERVAL = 1
+
+# Ограничения по времени (секунды) сообщений от пользователя
+MESSAGE_LIMIT = 2
+CALLBACK_LIMIT = 0.5
