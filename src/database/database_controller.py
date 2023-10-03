477db54d010416cce77da483e102ea81dde946c7
Hafurfur <rogmesec@yandex.ru>
2023-10-03 17:09:31 +0300

    Добавлены обязательные методы БД

--- src/database/database_controller.py
+++ src/database/database_controller.py
@@ -1,36 +1,47 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-from src.database.models import Base
+from typing import TYPE_CHECKING, Any
+from src.database.db_base import IndividualDBMethodsAbstr
+from src.database.base_db_controller import BaseDBController
 from src.logger.base_logger import log
 
-
 if TYPE_CHECKING:
-    from sqlalchemy import Engine
+    from sqlalchemy import Engine, CursorResult, Table, Column, Insert, Select, Update, Delete
 
 
-class DatabaseController:
+class DatabaseController(IndividualDBMethodsAbstr, BaseDBController):
     _instance: DatabaseController = None
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, **kwargs) -> DatabaseController:
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self, database):
-        log.debug(f'Создание экземпляра объекта {self.__class__.__name__}')
+    def __init__(self, database: IndividualDBMethodsAbstr | BaseDBController) -> None:
+        super().__init__()
+        log.debug(f'Создание объекта контролера БД {self.__class__.__name__}')
         self._db = database
-        self._engine = None
+
+    def create_engine(self, **kwargs: Any) -> None:
+        self._db.create_engine(**kwargs)
 
     def get_engine(self) -> Engine:
-        log.info('Получение движка SQLAlchemy')
-        if self._engine is None:
-            self._engine = self._db.get_engine()
+        return self._db.get_engine()
 
-        return self._engine
+    def create_tables(self) -> None:
+        self._db.create_tables()
 
-    def create_table(self):
-        log.info('Создание таблиц БД из модели')
-        if self._engine is None:
-            self.get_engine()
+    def insert(self, stmt: Insert, data: list[dict] | dict) -> None:
+        return self._db.insert(stmt, data)
 
-        Base.metadata.create_all(self._engine)
+    def insert_on_conflict_do_update(self, table: Table,
+                                     conflict_column: Column, data: dict) -> None:
+        self._db.insert_on_conflict_do_update(table, conflict_column, data)
+
+    def select(self, stmt: Select) -> CursorResult:
+        return self._db.select(stmt)
+
+    def update(self, stmt: Update, data: list[dict] | dict) -> None:
+        return self._db.update(stmt, data)
+
+    def delete(self, stmt: Delete) -> None:
+        return self._db.delete(stmt)
