aac347c38e6bc67f0333af3b2f14108b2baa1bcc
Hafurfur <rogmesec@yandex.ru>
2023-10-03 17:31:10 +0300

    Обновлен запрос к БД

--- src/scheduler/jobs/upd_readable_mg_job.py
+++ src/scheduler/jobs/upd_readable_mg_job.py
@@ -1,11 +1,10 @@
-from loader import scheduler, Session_db
+from loader import scheduler, db
 from src.configs.settings import UPD_READABLE_MB_JOB_INTERVAL
-from src.database.models import MangaAccounts, TrackedManga, MgAccountTrackedMgAssociationTable
+from src.database.models import manga_accounts, tracked_manga, readable_manga
 from src.tele_bot.bot_back.site_data import get_readable_mg_acc
 from src.logger.base_logger import log
 
 from sqlalchemy import select, delete, insert
-from sqlalchemy.exc import SQLAlchemyError, DBAPIError
 
 
 @scheduler.scheduled_job('interval', hours=UPD_READABLE_MB_JOB_INTERVAL)
@@ -20,11 +19,13 @@ def udp_readable_mg() -> None:
             log.debug(f'Ошибка при получении списка манги с сайта | status_code={status_code}')
             continue
 
-        all_tracked_mg = _get_tracked_mg_db()
+        if not readable_mg_acc_site:
+            continue
+
+        all_tracked_id_mg_db = _get_tracked_id_mg_db()
         readable_mg_acc_db = _get_readable_mg_acc_db(mg_acc)
 
-        new_tracked_mg = [_create_tracked_mg(manga) for manga in readable_mg_acc_site
-                          if manga.get('manga_id') not in all_tracked_mg]
+        new_tracked_mg = _create_tracked_mg_list(readable_mg_acc_site, all_tracked_id_mg_db)
         del_readable_mg = readable_mg_acc_db.difference(readable_mg_id_site)
         new_readable_mg = readable_mg_id_site.difference(readable_mg_acc_db)
 
@@ -33,22 +34,14 @@ def udp_readable_mg() -> None:
         _sav_new_readable_mg(new_readable_mg, mg_acc)
 
 
-def _save_mew_tracked_mg(manga: list) -> None:
+def _save_mew_tracked_mg(manga: list[dict]) -> None:
     log.debug(f'Сохранение новой отслеживаемой манги в БД | manga={manga}')
     if not manga:
         log.debug('Список новой манги пуст')
         return
 
-    with Session_db() as session:
-        try:
-            session.add_all(manga)
-            session.commit()
-        except (SQLAlchemyError, DBAPIError) as error:
-            log.error('Ошибка при сохранении новой отслеживаемой манги в БД (SQLAlchemy)', exc_info=error)
-            raise
-        except Exception as error:
-            log.error('Ошибка при сохранении новой отслеживаемой манги в БД', exc_info=error)
-            raise
+    stmt = insert(tracked_manga)
+    db.insert(stmt, manga)
 
 
 def _del_readable_mg(del_manga: set, mg_acc_id: int) -> None:
@@ -58,21 +51,11 @@ def _del_readable_mg(del_manga: set, mg_acc_id: int) -> None:
         log.debug('Список удаляемой читаемой манги у аккаунта пуст')
         return
 
-    with Session_db() as session:
-        try:
-            stmt = delete(MgAccountTrackedMgAssociationTable).where(
-                MgAccountTrackedMgAssociationTable.mg_acc_id == mg_acc_id).where(
-                MgAccountTrackedMgAssociationTable.tracked_mg_id.in_(del_manga))
+    stmt = delete(readable_manga).where(
+        readable_manga.c.mg_acc_id == mg_acc_id).where(
+        readable_manga.c.tracked_mg_id.in_(del_manga))
 
-            log.debug(f'Запрос = {stmt}')
-            session.execute(stmt)
-            session.commit()
-        except (SQLAlchemyError, DBAPIError) as error:
-            log.error('Ошибка при удаление читаемой манги у манга аккаунта из ДБ (SQLAlchemy)', exc_info=error)
-            raise
-        except Exception as error:
-            log.error('Ошибка при удаление читаемой манги у манга аккаунта из ДБ', exc_info=error)
-            raise
+    db.delete(stmt)
 
 
 def _sav_new_readable_mg(new_readable_mg: set, mg_acc_id: int) -> None:
@@ -84,81 +67,49 @@ def _sav_new_readable_mg(new_readable_mg: set, mg_acc_id: int) -> None:
         return
 
     list_rd_mg = [{'mg_acc_id': mg_acc_id, 'tracked_mg_id': mg_id} for mg_id in new_readable_mg]
-
-    with Session_db() as session:
-        try:
-            stmt = insert(MgAccountTrackedMgAssociationTable).values(list_rd_mg)
-            log.debug(f'Запрос = {stmt}')
-            session.execute(stmt)
-            session.commit()
-        except (SQLAlchemyError, DBAPIError) as error:
-            log.error('Ошибка при сохранении новой читаемой манги у манга аккаунта в ДБ (SQLAlchemy)', exc_info=error)
-            raise
-        except Exception as error:
-            log.error('Ошибка при сохранении новой читаемой манги у манга аккаунта в ДБ', exc_info=error)
-            raise
+    stmt = insert(readable_manga)
+    db.insert(stmt, list_rd_mg)
 
 
-def _create_tracked_mg(data: dict) -> TrackedManga:
-    log.debug(f'Создание объектов отслеживаемой манги | data={data}')
+def _create_tracked_mg_list(readable_mg_acc_site: list[dict], all_tracked_mg_db: set) -> list[dict]:
+    log.debug(f'Создание списка отслеживаемой манги')
 
-    last_chapter = data.get('last_chapter') if data.get('last_chapter') else {}
+    result = []
 
-    return TrackedManga(manga_id=data.get('manga_id'), slug=data.get('slug'), name_rus=data.get('rus_name'),
-                        cover_id=data.get('cover'), last_volume=last_chapter.get('volume'),
-                        last_chapter=last_chapter.get('number'))
+    for site_manga in readable_mg_acc_site:
+        if site_manga.get('manga_id') not in all_tracked_mg_db:
+            last_chapter = site_manga.get('last_chapter') if site_manga.get('last_chapter') else {}
+
+            data = {'id': site_manga.get('manga_id'), 'slug': site_manga.get('slug'),
+                    'name_rus': site_manga.get('rus_name'), 'cover_id': site_manga.get('cover'),
+                    'last_volume': last_chapter.get('volume'), 'last_chapter': last_chapter.get('number')}
+
+            result.append(data)
+
+    return result
 
 
 def _get_mg_acc_db() -> set:
     log.debug(f'Получение всех манга аккаунтов из ДБ')
-    with Session_db() as session:
-        try:
-            stmt = select(MangaAccounts.account_id)
-            log.debug(f'Запрос = {stmt}')
-            result = session.scalars(stmt).all()
-        except (SQLAlchemyError, DBAPIError) as error:
-            log.error('Ошибка при всех манга аккаунтов из ДБ (SQLAlchemy)', exc_info=error)
-            raise
-        except Exception as error:
-            log.error('Ошибка при всех манга аккаунтов из ДБ', exc_info=error)
-            raise
 
-    return set(result)
+    stmt = select(manga_accounts.c.id)
+    result = set(db.select(stmt).scalars().all())
+    return result
 
 
-def _get_tracked_mg_db() -> set:
-    log.debug(f'Получение всей отслеживаемой манги из ДБ')
-    with Session_db() as session:
-        stmt = select(TrackedManga.manga_id)
-        result = session.scalars(stmt).all()
+def _get_tracked_id_mg_db() -> set:
+    log.debug(f'Получение всех id отслеживаемой манги из ДБ')
 
-        try:
-            stmt = select(TrackedManga.manga_id)
-            log.debug(f'Запрос = {stmt}')
-            result = session.scalars(stmt).all()
-        except (SQLAlchemyError, DBAPIError) as error:
-            log.error('Ошибка при получение всей отслеживаемой манги из ДБ (SQLAlchemy)', exc_info=error)
-            raise
-        except Exception as error:
-            log.error('Ошибка при получение всей отслеживаемой манги из ДБ', exc_info=error)
-            raise
-
-    return set(result)
+    stmt = select(tracked_manga.c.id)
+    result = set(db.select(stmt).scalars().all())
+    return result
 
 
 def _get_readable_mg_acc_db(mg_acc_id: int) -> set:
     log.debug(f'Получение всей читаемой манги у манга аккаунта из ДБ | mg_acc_id={mg_acc_id}')
-    with Session_db() as session:
-        try:
-            stmt = select(TrackedManga.manga_id).join(MangaAccounts.readable_manga).where(
-                MangaAccounts.account_id == mg_acc_id)
-            log.debug(f'Запрос = {stmt}')
-            result = session.scalars(stmt).all()
-        except (SQLAlchemyError, DBAPIError) as error:
-            log.error('Ошибка при получение всей читаемой манги у манга аккаунта из ДБ (SQLAlchemy)', exc_info=error)
-            raise
-        except Exception as error:
-            log.error('Ошибка при получение всей читаемой манги у манга аккаунта из ДБ', exc_info=error)
-            raise
 
-    return set(result)
+    stmt = select(readable_manga.c.tracked_mg_id).join(manga_accounts).where(
+        manga_accounts.c.id == mg_acc_id)
+
+    result = set(db.select(stmt).scalars().all())
+    return result
