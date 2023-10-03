6b5589e43909acd648d65d0c8de567a4c05d10ee
Hafurfur <rogmesec@yandex.ru>
2023-10-03 17:29:51 +0300

    ~

--- start.py
+++ start.py
@@ -1,15 +1,13 @@
 from src.logger.base_logger import log
-import src.configs.settings
 from src.configs.settings import MESSAGE_LIMIT, CALLBACK_LIMIT
-from loader import scheduler, bot
+from loader import bot, scheduler
 import src.tele_bot.handlers
+import src.scheduler.jobs
 from src.tele_bot.middlewares.anti_spam import AntiSpam
-import src.scheduler
 
 
 def run():
     scheduler.start()
-
     bot.setup_middleware(AntiSpam(MESSAGE_LIMIT, CALLBACK_LIMIT))
 
     log.info('Старт бота')
