3f1e7ad6b1d49352d0f15e92b7569655ce59f8ac
Hafurfur <rogmesec@yandex.ru>
2023-10-03 17:26:47 +0300

    Обновлены запросы к БД

--- src/tele_bot/bot_back/save_tg_acc.py
+++ src/tele_bot/bot_back/save_tg_acc.py
@@ -1,28 +1,17 @@
-from src.database import TelegramAccounts
-from loader import Session_db
-from src.logger.base_logger import log
+from datetime import datetime
 
-from sqlalchemy import func
-from sqlalchemy.exc import SQLAlchemyError, DBAPIError
-from sqlalchemy.dialects.sqlite import insert
+from src.database import telegram_accounts
+from loader import db
+from src.logger.base_logger import log
 
 
 def save_telegram_account(account_id: int, username: str, first_name: str, second_name: str) -> None:
     log.info('Сохранение телеграмм аккаунта в БД')
-    log.debug(f'account_id={account_id}, username={username}, first_name={first_name}, second_name={second_name}')
-    with Session_db() as session:
-        try:
-            stmt = insert(TelegramAccounts).values(account_id=account_id, username=username, first_name=first_name,
-                                                   second_name=second_name, active=True)
-            stmt = stmt.on_conflict_do_update(index_elements=[TelegramAccounts.account_id],
-                                              set_=dict(username=username, first_name=first_name,
-                                                        second_name=second_name, update_date=func.now()))
-            log.debug(f'Запрос = {stmt}')
-            session.execute(stmt)
-            session.commit()
-        except (SQLAlchemyError, DBAPIError) as error:
-            log.error('Ошибка при сохранении телеграм аккаунта (SQLAlchemy)', exc_info=error)
-            raise
-        except Exception as error:
-            log.error('Ошибка при сохранении телеграм аккаунта', exc_info=error)
-            raise
+    log.debug(f'id={account_id}, username={username}, first_name={first_name}, second_name={second_name}')
+
+    data = {'insert_data': {'id': account_id, 'username': username, 'first_name': first_name,
+                            'second_name': second_name, 'active': True},
+            'update_data': {'update_date': datetime.utcnow(), 'username': username, 'first_name': first_name,
+                            'second_name': second_name, 'active': True}}
+
+    db.insert_on_conflict_do_update(telegram_accounts, telegram_accounts.c.id, data)
