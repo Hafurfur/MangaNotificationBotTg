c8b5514d3af5145b1c10aae10eb80f2d199b866f
Hafurfur <rogmesec@yandex.ru>
2023-10-03 17:12:22 +0300

    Удалена фабрика сессии для SQLAlchemy, добавлен путь к папке с данными

--- loader.py
+++ loader.py
@@ -1,13 +1,20 @@
 from os import getenv
-from src.database import DatabaseController, SqliteDB
+from src.database.database_controller import DatabaseController
+from src.database.sqlite_db import SqliteDB
+from src.configs.settings import DATA_DIR, ENGINE_SETTINGS
 
 from apscheduler.schedulers.background import BackgroundScheduler
-from sqlalchemy.orm import sessionmaker
 from telebot import TeleBot, StatePickleStorage
 
-db = DatabaseController(SqliteDB(getenv('SQLITE_DB_NAME'), truncate_microseconds=True))
-db.create_table()
-Session_db = sessionmaker(bind=db.get_engine())
+# проверка данных из .env
+
+db = DatabaseController(SqliteDB(data_dir=DATA_DIR, truncate_microseconds=True))
+db.create_engine(**ENGINE_SETTINGS)
+db.create_tables()
+
 bot = TeleBot(token=getenv('TOKEN'), skip_pending=True,
-              state_storage=StatePickleStorage(file_path='data/state-save/states.pkl'), use_class_middlewares=True)
+              state_storage=StatePickleStorage(file_path=f'{DATA_DIR}/state-save/states.pkl'),
+              use_class_middlewares=True)
 scheduler = BackgroundScheduler()
+
+print()
